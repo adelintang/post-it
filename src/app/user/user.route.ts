643606cc15617765c935f76ca53aa363f4aea225
adelintang <adelintang157@gmail.com>
2024-11-05 00:56:45 +0700

    feat: user

--- src/app/user/user.route.ts
+++ src/app/user/user.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 
+import { hasRole } from '../../middleware'
 import { catchAsync } from '../../utils'
 
 import { getUsers, getUser } from './user.controller'
 
 const route = Router()
 
-route.get('/:userId', catchAsync(getUser))
-route.get('/', catchAsync(getUsers))
+route.get('/:userId', hasRole('ADMIN'), catchAsync(getUser))
+route.get('/', hasRole('ADMIN'), catchAsync(getUsers))
 
 export default route
