4dd52eb1850f923feecd331402fe2f41d6729710
adelintang <adelintang157@gmail.com>
2024-11-07 21:42:27 +0700

    build: update schema and prisma client

---
+++ prisma/migrations/20241107143642_delete_profileid_in_follow_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `profile_id` on the `Follow` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Follow" DROP COLUMN "profile_id";
